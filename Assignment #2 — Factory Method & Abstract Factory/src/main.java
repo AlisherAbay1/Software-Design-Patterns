7485c11f777f16013427a9e6ce5c3d8fc97158ff
Alisher <alisherggcom@gmail.com>
2026-09-15 15:52:49 +0500

    feat: add usage example

--- Assignment #2 — Factory Method & Abstract Factory/src/main.java
+++ Assignment #2 — Factory Method & Abstract Factory/src/main.java
@@ -1,3 +1,28 @@
-public class main {
-    
+import fabric_method.*;
+import fabric_method.factories.ShipFactory;
+import fabric_method.factories.TruckFactory;
+
+public class Main {
+    static TransportFactory factory;
+
+    public static void main(String[] args) {
+        // Fabric method
+        String[] configValues = {"Truck", "Ship"};
+        for (String config: configValues) {
+            createFactory(config);
+            String objectId = "123";
+            Double deliveryCost = factory.deliver(objectId, 5);
+            System.out.println(config + "'s' delivery cost for " + objectId + " is equal to " + deliveryCost + "$");
+        }
+    }
+
+    static void createFactory(String config) {
+        if (config.equals("Ship")) {
+            factory = new ShipFactory();
+        } else if (config.equals("Truck")) {
+            factory = new TruckFactory();
+        } else {
+            throw new IllegalArgumentException("Uncorrect transport type");
+        }
+    }
 }
